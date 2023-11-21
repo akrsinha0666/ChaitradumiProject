97542c7c86e625287b05803e5caebf27a42d0737
Abhishek <akrsinha06@gmail.com>
2023-11-21 13:33:53 +0530

    DEmo class pushed

---
+++ src/test/java/demo11/DemoGithub.java
@@ -0,0 +1,16 @@
+package demo11;
+
+public class DemoGithub {
+public static void main(String[] args) {
+	int a = 10;
+	int b= 20 ;
+	int c= a+b;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(a-b);
+	System.out.println(b-a);
+	System.out.println(a*b);
+	System.out.println(c*a);
+}
+}
