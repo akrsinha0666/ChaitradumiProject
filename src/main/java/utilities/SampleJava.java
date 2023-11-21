7cd931458d312a99313d30b98801b70bec7f1442
Abhishek <akrsinha06@gmail.com>
2023-11-21 15:21:42 +0530

    new message

--- src/main/java/utilities/SampleJava.java
+++ src/main/java/utilities/SampleJava.java
@@ -1,10 +1,7 @@
 package utilities;
 
 public class SampleJava {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- System.out.println("Hellooooo");
-	}
-
+ public static void main(String[] args) {
+	System.out.println("hiiiwee");
+}
 }
